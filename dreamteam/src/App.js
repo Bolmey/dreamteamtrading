56517f5d6ea668342e8ba2f98bd1fd95083a69d6
jesper <jesperbennett@protonmail.com>
2021-07-01 11:22:44 -0400

    wasd

--- dreamteam/src/App.js
+++ dreamteam/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom'
 import home from './components/home/home.js'
+import signup from './components/signup/signup'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       
       <Switch>
         <Route exact path="/" component={home}></Route>
+        <Route exact path="/signup" component={signup}></Route>
       </Switch>
       
     </div>
