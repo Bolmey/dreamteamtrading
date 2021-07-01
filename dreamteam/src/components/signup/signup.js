db3b80d8b5db968838531bd33e2a7da535a63025
Juan Bolmey <72952859+Bolmey@users.noreply.github.com>
2021-07-01 14:33:38 -0400

    Update

--- dreamteam/src/components/signup/signup.js
+++ dreamteam/src/components/signup/signup.js
@@ -1,4 +1,4 @@
-import  { React, useState } from 'react'
+import { React, useState } from 'react'
 import './signup.css'
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
@@ -15,13 +15,13 @@ export default function Signup() {
         e.preventDefault();
 
         emailjs.sendForm('service_jbfyl7l', 'template_m7i2ujf', e.target, 'user_VYHq6LD11j55pFnsnL1gZ')
-          .then((result) => {
-              console.log(result.text);
-          }, (error) => {
-              console.log(error.text);
-          });
-          e.target.reset()
-      }
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset()
+    }
 
 
 
@@ -35,20 +35,20 @@ export default function Signup() {
                 <div className="formDiv">
                     <form onSubmit={sendEmail} className="form">
 
-                        <label>Name</label><br/>
-                        <input type="text" name="user_name" /><br/><br/>
-                        <label>Email</label><br/>
-                        <input type="email" name="user_email" /><br/><br/>
-                        <label>Message</label><br/>
-                        <textarea name="message" >Default Message In Here.. I want to join please send invoice</textarea><br/><br/>
-                        <input onClick={ () => setShow(true) } type="submit" value="Send" />
-                        
+                        <label>Name</label><br />
+                        <input type="text" name="user_name" /><br /><br />
+                        <label>Email</label><br />
+                        <input type="email" name="user_email" /><br /><br />
+                        <label>Message</label><br />
+                        <textarea name="message" >Default Message In Here.. I want to join please send invoice</textarea><br /><br />
+                        <input onClick={() => setShow(true)} type="submit" value="Send" />
+
 
                     </form>
                 </div>
 
-                <button onClick={ () => setShow(true) }>Open Model</button>
-                <Modal onClose={ () => setShow(false) } show={show} />
+                <button onClick={() => setShow(true)}>Open Model</button>
+                <Modal onClose={() => setShow(false)} show={show} />
 
                 <h1>What's Next?</h1>
                 <p>Once you submit your Name, Email, and Message we will reply back with an Invoice.</p>
