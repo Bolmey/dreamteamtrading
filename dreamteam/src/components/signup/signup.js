055e612f853620851890e3c52384a13d5df32d18
Juan Bolmey <72952859+Bolmey@users.noreply.github.com>
2021-07-01 13:16:47 -0400

    sign up page update

--- dreamteam/src/components/signup/signup.js
+++ dreamteam/src/components/signup/signup.js
@@ -10,16 +10,16 @@ export default function signup() {
 
     function sendEmail(e) {
         e.preventDefault();
-    
+
         emailjs.sendForm('service_jbfyl7l', 'template_m7i2ujf', e.target, 'user_VYHq6LD11j55pFnsnL1gZ')
-          .then((result) => {
-              console.log(result.text);
-              console.log(e.target)
-          }, (error) => {
-              console.log(error.text);
-          });
-          e.target.reset()
-      }
+            .then((result) => {
+                console.log(result.text);
+                console.log(e.target)
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset()
+    }
 
 
 
@@ -28,24 +28,25 @@ export default function signup() {
         <div className="signupBody">
 
             <section className="signupSection">
+                <h1 className="joinHeader">Join the Dream Team Today!</h1>
 
                 <div className="formDiv">
                     <form onSubmit={sendEmail} className="form">
 
-                        <label>Name</label><br/>
-                        <input type="text" name="user_name" /><br/><br/>
-                        <label>Email</label><br/>
-                        <input type="email" name="user_email" /><br/><br/>
-                        <label>Message</label><br/>
-                        <textarea name="message" >Default Message In Here.. I want to join please send invoice</textarea><br/><br/>
-                        <input type="submit" value="Send" />
+                        <label>Name</label><br />
+                        <input type="text" name="user_name" /><br /><br />
+                        <label>Email</label><br />
+                        <input type="email" name="user_email" /><br /><br />
+                        <label>Message</label><br />
+                        <textarea name="message">I want to join please send invoice</textarea><br /><br />
+                        <input className='submitButton' type="submit" value="Send" />
 
                     </form>
                 </div>
 
 
-                <h1>DreamTrading</h1>
-                <p>Text and stuff and forms and signups and pay please, thanks</p>
+                <h1>What's Next?</h1>
+                <p>Once you submit your Name, Email, and Message we will reply back with an Invoice.</p>
             </section>
 
 
