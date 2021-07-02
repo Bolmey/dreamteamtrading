18800cc98da4901ab382b761d3658ec8b3bc69f4
jesper <jesperbennett@protonmail.com>
2021-07-02 11:37:54 -0400

    wasd

--- dreamteam/src/components/footer/footer.js
+++ dreamteam/src/components/footer/footer.js
@@ -4,7 +4,9 @@ import './footer.css'
 export default function Footer() {
     return (
         <div className="footerSection">
-                <h2>Footer - Email - OtherLink - Phonenumber - Stuff</h2>
+                <h4>Dream Team Trading</h4>
+                <h5>Dreamteamtradingfx@gmail.com</h5>
+                <a className="instagramLink" href="https://www.instagram.com/dream_team_trading/"/>
         </div>
     )
 }
