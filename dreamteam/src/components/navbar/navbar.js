202ebcf93de5c865a66b136066d5a2cd19b85b47
Juan Bolmey <72952859+Bolmey@users.noreply.github.com>
2021-07-02 11:37:42 -0400

    color to nav and reposition

--- dreamteam/src/components/navbar/navbar.js
+++ dreamteam/src/components/navbar/navbar.js
@@ -3,14 +3,14 @@ import './navbar.css'
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from "react-router-dom";
 
 export default function NavbarSection() {
     return (
         <div className='NavbarContainer'>
             <Navbar collapseOnSelect expand="lg" className="navbarColor">
                 <Container>
-                    <Navbar className='navName' href="#home">Dream Team Trading</Navbar>
+                    <Link to="/"><Navbar className='navName' href="#home">Dream Team Trading</Navbar></Link>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav>
